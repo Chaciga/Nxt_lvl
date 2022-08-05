48fb0d054bec0b1a925183270d28c9c714b3bfc8
Chaciga <emmachaciga@gmail.com>
2022-08-05 18:06:52 +0300

    portfolio page

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let filterContainer = document.querySelector(".gallary-filter")
 
 filterContainer.addEventListener("click", (event) => {
     if (event.target.classList.contains("filter-item")){
-        
-        filterContainer.querySelector("filter-item").classList.remove("active");
+        // deactivates existing active filter item
+        filterContainer.querySelector(".active").classList.remove("active");
         // this activates the new filter item
         event.target.classList.add("active");
     }
