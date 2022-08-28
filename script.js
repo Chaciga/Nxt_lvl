7f07d9c86636e1119866976a56db361d7283c242
Chaciga <emmachaciga@gmail.com>
2022-08-28 21:12:59 +0300

    Final design of the website

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ window.addEventListener('scroll', function(){
 // Portifolio Gallary
 
 let filterContainer = document.querySelector(".gallary-filter")
+let gallaryItems = document.querySelectorAll(".gallary-item")
 
 filterContainer.addEventListener("click", (event) => {
     if (event.target.classList.contains("filter-item")){
@@ -15,5 +16,36 @@ filterContainer.addEventListener("click", (event) => {
         filterContainer.querySelector(".active").classList.remove("active");
         // this activates the new filter item
         event.target.classList.add("active");
+         
+        let filterValue = event.target.getAttribute("data-filter");
+        gallaryItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
+
+
+// Review Carosel
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1200:{
+            items:2
+        }
     }
 })
